cf3508f1a13fa40252d9ba30349bdacd2f50d32e
joo-young-song <rhshrhsh96@gmail.com>
2018-09-17 21:49:24 +0900

    drill 05 - make base
    기초 뼈대 제작

---
+++ Drills/drill 05/drill05.py
@@ -0,0 +1,46 @@
+from pico2d import *
+
+open_canvas()
+
+character = load_image('animation_sheet.png')
+
+def move_character_1():
+    pass
+def move_character_2():
+    pass
+def move_character_3():
+    pass
+def move_character_3():
+    pass
+def move_character_4():
+    pass
+def move_character_5():
+    pass
+def move_character_6():
+    pass
+def move_character_7():
+    pass
+def move_character_8():
+    pass
+def move_character_9():
+    pass
+def move_character_10():
+    pass
+
+
+
+while True:
+    move_character_1()
+    move_character_2()
+    move_character_3()
+    move_character_4()
+    move_character_5()
+    move_character_6()
+    move_character_7()
+    move_character_8()
+    move_character_9()
+    move_character_10()
+
+
+    
+close_canvas()
