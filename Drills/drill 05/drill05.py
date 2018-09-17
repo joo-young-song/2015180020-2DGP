b7be3f29dc9da140eaf174cc0d89eebd5986af4b
joo-young-song <rhshrhsh96@gmail.com>
2018-09-17 22:22:20 +0900

    drill05 move_character 완성
    캐릭터 움직임 함수1 완성

--- Drills/drill 05/drill05.py
+++ Drills/drill 05/drill05.py
@@ -2,10 +2,26 @@ from pico2d import *
 
 open_canvas()
 
-character = load_image('animation_sheet.png')
+character = load_image("animation_sheet.png")
+
+
 
 def move_character_1():
-    pass
+    x ,y= 203,535
+    frame = 0
+    x3,y3 = (132-203)/10 , (243-535)/10
+    i = 0
+    while i < 10:
+        clear_canvas_now()
+        frame = (frame + 1) % 8
+        character.clip_draw(frame * 100, 0, 100, 100, x, y)
+        update_canvas()
+        x = x + x3
+        y = y + y3
+        i += 1
+        delay(0.05)
+
+    
 def move_character_2():
     pass
 def move_character_3():
