3ba1a876eb4a9024e5104d90231cfe3d8e701968
joo-young-song <rhshrhsh96@gmail.com>
2018-10-14 23:54:06 +0900

    2 주차 적 클래스 완성
    적 클래스 완성

--- 2DGP project/2주차 적구현/enemy_base.py
+++ 2DGP project/2주차 적구현/enemy_base.py
@@ -1,5 +1,6 @@
 from pico2d import *
 import os
+import math
 
 os.chdir('D:\\2DGM\\2DGP\\2015180020-2DGP\\2DGP project\\2주차 적구현')
 
@@ -10,25 +11,22 @@ open_canvas()
 
 class enemy:
     def __init__(self):
-        self.x = 300
-        self.y = 0
+        self.x = 0
+        self.y = 300
         self.hp = 10
-        self.head = 1
-        self.Xmove = 0
-        self.Ymove = 1
+        self.radians = 0.0
+        self.Xmove = 0.0
+        self.Ymove = 1.0
+        self.head = 0
         self.frame = 0
         self.image = load_image('pig_first.png')
     def update(self):
         self.frame = (self.frame + 1) % 8
-        self.x = self.x + self.Xmove
-        self.y = self.y + self.Ymove
+        self.x = self.x + 5*math.cos(self.radians)
+        self.y = self.y + 5*math.sin(self.radians)
         delay(0.05)
     def draw(self):
-        #self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
-        if self.head == 0:
-            self.image.rotate_draw(0,self.x,self.y)
-        elif self.head == 1:
-            self.image.rotate_draw(1.57,self.x,self.y)
+        self.image.rotate_draw(self.radians,self.x,self.y)
 
 
 pig = enemy()
