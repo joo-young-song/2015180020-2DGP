39c59341c251a4ce6afa565a3d45e9062e6a3af6
joo-young-song <rhshrhsh96@gmail.com>
2018-10-21 21:54:42 +0900

    3주차 스테이지 맵 타일 위치 리스트 완성
    리스트를 이용하여 타일 위치 제작 완료

--- 2DGP project/3주차 맵구현/map_stage_1.py
+++ 2DGP project/3주차 맵구현/map_stage_1.py
@@ -1,5 +1,20 @@
 from pico2d import *
 
+
+tile_rotate = [[0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0],
+               [0,0,0,1,1,0,0,0,0,0,1,1,1,0,0,0]
+]
+
 open_canvas(800, 600)
 
 grass = None
@@ -8,18 +23,27 @@ enemy_tile = None
 
 class Grass:
     def __init__(self):
-        self.x = 400
-        self.y = 300
         self.image = load_image('grass1.jpg')
-
     def draw(self):
-        self.image.draw(0, 0, 100, 100)
+        for x in range(16):
+            for y in range(12):
+                if tile_rotate[y][x] == 0 :
+                    self.image.draw(x * 50 + 25, y * 50 + 25, 50, 50)
+
+class Tile:
+    def __init__(self):
+        self.image = load_image('tile1.jpg')
+    def draw(self):
+        for x in range(16):
+            for y in range(12):
+                if tile_rotate[y][x] == 1 :
+                    self.image.rotate_draw(0, x * 50 + 25, y * 50 + 25, 50, 50)
+
 
 
 grass = Grass()
 
-
-
+enemy_tile = Tile()
 
 
 while True:
@@ -27,5 +51,7 @@ while True:
     clear_canvas()
 
     grass.draw()
+
+    enemy_tile.draw()
 
     update_canvas()
