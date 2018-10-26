f488bc120fbcd37d49cf2b11b05f13e07e6a1b92
joo-young-song <rhshrhsh96@gmail.com>
2018-10-27 02:55:18 +0900

    적 움직임 완료
    스테이지 리스트에 숫자를 이용하여 방향 할당

--- 2DGP project/2주차 적구현/enemy_base.py
+++ 2DGP project/2주차 적구현/enemy_base.py
@@ -26,6 +26,7 @@ class enemy:
         self.radians = 0.0
         self.head = 0
         self.frame = 0
+        self.reflect = ''
         if enemy.image is None:
             enemy.image = load_image('stage1_pig1.png')
 
@@ -35,22 +36,18 @@ class enemy:
         self.x = self.x + 5 * math.cos(self.radians)
         self.y = self.y + 5 * math.sin(self.radians)
 
-        if self.x + 50 * math.cos(self.radians) > 800:
-            self.radians = self.radians - 1
-        elif self.x + 50 * math.cos(self.radians) < 0:
-            self.radians = self.radians - 1
-        elif self.y + 50 * math.sin(self.radians) > 600:
-            self.radians = self.radians - 1
-        elif self.y + 50 * math.sin(self.radians) < 0:
-            self.radians = self.radians - 1
-        elif map_stage_2.tile_rotate[int((self.y + 50 * math.sin(self.radians)) // 50)][
-            int((self.x + 50 * math.cos(self.radians)) // 50)] == 0:
-            self.radians = self.radians - 1
-
+        if map_stage_2.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 1:
+            self.radians = 3.14
+        elif map_stage_2.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 2:
+            self.radians = -1.57
+        elif map_stage_2.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 3:
+            self.radians = 0
+        elif map_stage_2.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 4:
+            self.radians = 1.57
         delay(0.05)
 
     def draw(self):
-        self.image.clip_composite_draw(0, 100 + 100 * self.frame, 100, 100, self.radians, '', self.x, self.y, 100, 100)
+        self.image.clip_composite_draw(0, 100 + 100 * self.frame, 100, 100, self.radians, self.reflect, self.x, self.y, 100, 100)
 
 
 pig = enemy()
