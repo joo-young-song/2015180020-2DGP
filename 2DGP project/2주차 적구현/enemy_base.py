591a54346324fada2c6af0025111333d14da7ffe
joo-young-song <rhshrhsh96@gmail.com>
2018-10-27 01:59:47 +0900

    2~3week 점검
    스테이지 길에 따라 적의 움직임을 구현 중... 계산식 정리

--- 2DGP project/2주차 적구현/enemy_base.py
+++ 2DGP project/2주차 적구현/enemy_base.py
@@ -1,32 +1,53 @@
 from pico2d import *
 import os
 import math
+import map_stage_1
+import map_stage_2
+import map_stage_3
+
 
 os.chdir('D:\\2DGM\\2DGP\\2015180020-2DGP\\2DGP project\\2주차 적구현')
 
 
-pig = None
-
 open_canvas()
 
+pig = None
+
+Grass = map_stage_2.Grass()
+
+Tile = map_stage_2.Tile()
+
+
 class enemy:
+    image = None
     def __init__(self):
         self.x = 0
-        self.y = 300
+        self.y = 550
         self.hp = 10
         self.radians = 0.0
         self.Xmove = 0.0
         self.Ymove = 1.0
         self.head = 0
         self.frame = 0
-        self.image = load_image('pig_first.png')
+        self.Tile_Spot_x = 0
+        self.Tile_Spot_y = 0
+
+        if enemy.image == None:
+            enemy.image = load_image('stage1_pig1.png')
     def update(self):
-        self.frame = (self.frame + 1) % 8
-        self.x = self.x + 5*math.cos(self.radians)
-        self.y = self.y + 5*math.sin(self.radians)
+        self.frame = (self.frame + 1) % 7
+
+        self.x = self.x + 5 * math.cos(self.radians)
+        self.y = self.y + 5 * math.sin(self.radians)
+
+
+       
+
+
         delay(0.05)
     def draw(self):
-        self.image.rotate_draw(self.radians,self.x,self.y)
+        self.image.clip_composite_draw(0,100 + 100*self.frame,100,100,self.radians,'',self.x, self.y,100,100)
+
 
 
 pig = enemy()
@@ -38,6 +59,10 @@ pig = enemy()
 while True:
 
     clear_canvas()
+
+    Grass.draw()
+
+    Tile.draw()
 
     pig.draw()
 
