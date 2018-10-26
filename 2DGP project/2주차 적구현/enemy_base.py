714a6aff42a1edf8573f3052e3cc25f282f83cb9
joo-young-song <rhshrhsh96@gmail.com>
2018-10-27 02:08:25 +0900

    update 에 다음 위치가 안전한지 평가
    다음 이동할 위치가 안정적인지 평가하는 if elif 문 추가

--- 2DGP project/2주차 적구현/enemy_base.py
+++ 2DGP project/2주차 적구현/enemy_base.py
@@ -5,9 +5,7 @@ import map_stage_1
 import map_stage_2
 import map_stage_3
 
-
 os.chdir('D:\\2DGM\\2DGP\\2015180020-2DGP\\2DGP project\\2주차 적구현')
-
 
 open_canvas()
 
@@ -20,44 +18,44 @@ Tile = map_stage_2.Tile()
 
 class enemy:
     image = None
+
     def __init__(self):
         self.x = 0
         self.y = 550
         self.hp = 10
         self.radians = 0.0
-        self.Xmove = 0.0
-        self.Ymove = 1.0
         self.head = 0
         self.frame = 0
-        self.Tile_Spot_x = 0
-        self.Tile_Spot_y = 0
-
-        if enemy.image == None:
+        if enemy.image is None:
             enemy.image = load_image('stage1_pig1.png')
+
     def update(self):
         self.frame = (self.frame + 1) % 7
 
         self.x = self.x + 5 * math.cos(self.radians)
         self.y = self.y + 5 * math.sin(self.radians)
 
-
-       
-
+        if self.x + 50 * math.cos(self.radians) > 800:
+            pass
+        elif self.x + 50 * math.cos(self.radians) < 0:
+            pass
+        elif self.y + 50 * math.sin(self.radians) > 600:
+            pass
+        elif self.y + 50 * math.sin(self.radians) < 0:
+            pass
+        elif map_stage_2.tile_rotate[int((self.y + 50 * math.sin(self.radians)) // 50)][
+            int((self.x + 50 * math.cos(self.radians)) // 50)] == 0:
+            pass
 
         delay(0.05)
-    def draw(self):
-        self.image.clip_composite_draw(0,100 + 100*self.frame,100,100,self.radians,'',self.x, self.y,100,100)
 
+    def draw(self):
+        self.image.clip_composite_draw(0, 100 + 100 * self.frame, 100, 100, self.radians, '', self.x, self.y, 100, 100)
 
 
 pig = enemy()
 
-
-
-
-
 while True:
-
     clear_canvas()
 
     Grass.draw()
