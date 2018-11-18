0ff93bea84ce308c9149fd7e9c7234881f3cc9ed
joo-young-song <rhshrhsh96@gmail.com>
2018-11-19 01:47:29 +0900

    week7 두번째 적 제작
    두번째 적 제작 완료

---
+++ 2DGP project/2DGP 게임 제작/enemy_stage_1_2.py
@@ -0,0 +1,65 @@
+from pico2d import *
+
+import math
+
+import map_stage_1
+
+import game_world
+
+open_canvas()
+
+pig = None
+
+class enemy:
+    image = None
+
+    def __init__(self, show = 0):
+        self.x = -100
+        self.y = 300
+        self.hp = 40
+        self.radians = 0.0
+        self.head = 0
+        self.frame = 0
+        self.reflect = ''
+        self.count = show
+        if enemy.image is None:
+            enemy.image = load_image('enemy_image//stage1_pig2.png')
+
+    def update(self):
+        if self.hp == 0:
+            game_world.remove_object(self)
+        if self.count == 0 :
+            if self.x > 0 :
+                self.frame = (self.frame + 1) % 5
+
+                self.x = self.x + 3 * math.cos(self.radians)
+                self.y = self.y + 3 * math.sin(self.radians)
+
+                if map_stage_1.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 1:
+                    self.radians = 3.14
+                    self.reflect = "hv"
+                elif map_stage_1.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 2:
+                    self.radians = -1.57
+                    self.reflect = ''
+                elif map_stage_1.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 3:
+                    self.radians = 0
+                    self.reflect = ''
+                elif map_stage_1.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 4:
+                    self.radians = 1.57
+                    self.reflect = ''
+                elif map_stage_1.tile_rotate[int(self.y // 50)][int(self.x // 50)] == 5:
+                    game_world.remove_object(self)
+
+
+            else :
+                self.x = self.x + 5 * math.cos(self.radians)
+                self.y = self.y + 5 * math.sin(self.radians)
+
+        else :
+            self.count -= 1
+
+    def draw(self):
+        self.image.clip_composite_draw(0, 43 * self.frame, 51, 43, self.radians, self.reflect, self.x, self.y, 50, 50)
+
+    def get_bb(self):
+        return self.x, self.y
