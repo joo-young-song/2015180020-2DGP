86a2a501c275c31f68689985ba44bcfa7e26d302
joo-young-song <rhshrhsh96@gmail.com>
2018-11-20 18:08:34 +0900

    week8 공격 이미지 프레임 변경
    프레임 오류 해결

--- 2DGP project/2DGP 게임 제작/white_attack.py
+++ 2DGP project/2DGP 게임 제작/white_attack.py
@@ -17,8 +17,8 @@ class fire:
             fire.image = load_image('bullet_image//attack_1.png')
 
     def update(self):
-        self.x = self.x + 100*math.cos(self.radians)*game_framework.frame_time
-        self.y = self.y + 100*math.sin(self.radians)*game_framework.frame_time
+        self.x = self.x + 250*math.cos(self.radians)*game_framework.frame_time
+        self.y = self.y + 250*math.sin(self.radians)*game_framework.frame_time
         self.frame = (self.frame+6*game_framework.frame_time) % 3
         for gets in game_world.enemy_objects():
             if gets.hp >= 0:
@@ -31,6 +31,6 @@ class fire:
 
 
     def draw(self):
-        self.image.clip_composite_draw(0, 16 + 16 + int(self.frame), 16, 16, self.radians, self.reflect, self.x, self.y, 10, 10)
+        self.image.clip_composite_draw(0, 16 * int(self.frame), 16, 16, self.radians, self.reflect, self.x, self.y, 20, 20)
 
 
